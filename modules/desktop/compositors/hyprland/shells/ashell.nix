9b22e1b0ac857532241ec73b6f5690a1e3957ee3
simi <simi.git@outlook.com>
2026-06-13 12:46:09 +0100

    Fix Desktop Services

--- modules/desktop/compositors/hyprland/shells/ashell.nix
+++ modules/desktop/compositors/hyprland/shells/ashell.nix
@@ -21,8 +21,19 @@
               hyprlock.enable = true;
             };
 
-            services.mako.enable = true;
-            
+            services = {
+              mako.enable = true;
+              elephant.enable = true;
+              walker = {
+                enable = true;
+                enableElephantIntegration = true;
+              };
+            };
+            systemd.user.services = { # Allow Home-Manager to manage settings for these services, But Don't Make SystemD Services.
+              mako = lib.mkForce {};
+              elephant = lib.mkForce {};
+              walker = lib.mkForce {};
+            };
 
             # Binds
             wayland.windowManager.hyprland.settings = {
@@ -32,7 +43,7 @@
                 "uwsm app -- ${lib.getExe pkgs.elephant}"
                 "uwsm app -- ${lib.getExe pkgs.walker} --gapplication-service"
               ];
-              
+
               bind = [
                 "SUPER, R, exec, ${lib.getExe pkgs.walker}"
               ];
