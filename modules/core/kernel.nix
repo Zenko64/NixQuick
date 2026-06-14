f4587bfde77d5fd4d8743726daa5cb56365400a3
simi <simi.git@outlook.com>
2026-06-14 15:04:57 +0100

    Fix: Nixos-hardware mkdefaults conflict with core kernel mkDefault, change to lower priority mkOptionDefault to avoid conflict

--- modules/core/kernel.nix
+++ modules/core/kernel.nix
@@ -1,6 +1,6 @@
 # Kernel Module
 {
   flake.modules.nixos.core = { lib, pkgs, ... }: {
-    boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+    boot.kernelPackages = lib.mkOptionDefault pkgs.linuxPackages_latest;
   };
 }
