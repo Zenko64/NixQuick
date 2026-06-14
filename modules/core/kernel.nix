a189e05541bbcff1560487a6a8597cd5d280fa5e
simi <simi.git@outlook.com>
2026-06-14 11:01:42 +0100

    Fix missing import

--- modules/core/kernel.nix
+++ modules/core/kernel.nix
@@ -1,6 +1,6 @@
 # Kernel Module
 {
-  flake.modules.nixos.core = { pkgs, ... }: {
+  flake.modules.nixos.core = {lib, pkgs, ... }: {
     boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
   };
 }
